f8001d8365c7c13f16648868d699e0b5119dfd45
Marta Adamczak <adamczak7@onet.eu>
2020-07-01 13:47:07 +0200

    Commit message

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String myName = "Marta";
+        String myHobby = "Basketball";
+        int favoriteNumber = 13;
+        boolean isClever = true;
+        double height = 1.69;
+        char firstLetterSurname = 'A';
+        System.out.println(myName);
+        System.out.println(myHobby);
+        System.out.println(favoriteNumber);
+        System.out.println(isClever);
+        System.out.println(height);
+        System.out.println(firstLetterSurname);
+    }
+}
